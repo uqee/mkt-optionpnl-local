1c912241b6690c40ad69296a67482ff5026b04dd
Denis Zhbankov <denis.zhbankov@gmail.com>
2015-12-17 20:54:55 +0300

    snapshot, subscribe: log errors

--- io.js
+++ io.js
@@ -15,7 +15,8 @@ module.exports = function (server) {
     socket.on('/ib/snapshot', function (msg) {
       log.print(log.LVL_DETAILS, 'client #' + socket.id, '/ib/snapshot', null, msg.contract);
       ib.mktdata.snapshot(msg.contract, function (err, res) {
-        log.print(log.LVL_DETAILS, 'server', '/ib/snapshot', JSON.stringify(res), msg.contract);
+        if (err) log.print(log.LVL_ERROR, 'server', '/ib/snapshot', err, msg.contract);
+        else log.print(log.LVL_DETAILS, 'server', '/ib/snapshot', JSON.stringify(res), msg.contract);
         socket.emit('/ib/snapshot', { req: msg, res: res });
       });
     });
@@ -23,7 +24,8 @@ module.exports = function (server) {
     socket.on('/ib/subscribe', function (msg) {
       log.print(log.LVL_DETAILS, 'client #' + socket.id, '/ib/subscribe', null, msg.contract);
       ib.mktdata.subscribe(msg.contract, function (err, res) {
-        log.print(log.LVL_DETAILS, 'server', '/ib/subscribe', JSON.stringify(res), msg.contract);
+        if (err) log.print(log.LVL_ERROR, 'server', '/ib/subscribe', err, msg.contract);
+        else log.print(log.LVL_DETAILS, 'server', '/ib/subscribe', JSON.stringify(res), msg.contract);
         socket.emit('/ib/subscribe', { req: msg, res: res });
       });
     });
