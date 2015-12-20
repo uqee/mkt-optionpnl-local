6443580bcedac0eccea060229ce841fe0d9eeddd
Denis Zhbankov <denis.zhbankov@gmail.com>
2015-12-20 12:19:54 +0300

    ib: log errors with codes >= 2000 with level lvl_xxl

--- api/ib.js
+++ api/ib.js
@@ -97,7 +97,7 @@ var ib = new (require('ib'))({
   
   ib.on('error', function (err, data) {
     var msg = ('' + err).substring(7 /* removes 'Error: ' */);
-    var lvl = (data.code >= 2000 ? log.LVL_INFO : (data.code >= 1000 ? log.LVL_WARNING : log.LVL_ERROR));
+    var lvl = (data.code >= 2000 ? log.LVL_XXL : (data.code >= 1000 ? log.LVL_WARNING : log.LVL_ERROR));
     log.print(lvl, 'ib', msg, JSON.stringify(data));
 
     var id = data.id;
